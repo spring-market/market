c379998142e7af4fdac4012bdb9feb6b6f0ac8ff
KimSuhyun <suhun1054@gmail.com>
2023-07-11 15:26:42 +0900

    update: STS test

--- src/main/java/dev/market/spring_market/SpringMarketApplication.java
+++ src/main/java/dev/market/spring_market/SpringMarketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringMarketApplication {
 
 	public static void main(String[] args) {
+		System.out.println("################# ¼º°ø");
 		SpringApplication.run(SpringMarketApplication.class, args);
 	}
 
