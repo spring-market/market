b58adaef76a6d00d54883601d63905e8899bc2eb
MSlcim <lciminsu@gmail.com>
2023-07-12 18:26:47 +0900

    fix:회원가입 시 비밀번호 암호화.

--- src/main/java/dev/market/spring_market/controller/UserController.java
+++ src/main/java/dev/market/spring_market/controller/UserController.java
@@ -15,21 +15,21 @@ public class UserController {
 
 
     //회원 마이페이지 정보 반환API
-   @GetMapping("/{userId}")
-    public UserResponse getUserInfo(@PathVariable Long userId){
-       System.out.println("컨트롤러 실행");
-       return userService.findById(userId);
-   }
+    @GetMapping("/{userId}")
+    public UserResponse getUserInfo(@PathVariable Long userId) {
+        System.out.println("컨트롤러 실행");
+        return userService.findById(userId);
+    }
 
-   @PatchMapping("/{userId}")
+    @PatchMapping("/{userId}")
     public UserResponse deleteUserInfo(@PathVariable Long userId) {
 
-       return userService.deleteUser(userId);
-   }
+        return userService.deleteUser(userId);
+    }
 
-   @PostMapping("/register")
-    public UserResponse registerUser(@RequestBody UserRequest userRequest){
+    @PostMapping("/register")
+    public UserResponse registerUser(@RequestBody UserRequest userRequest) {
         UserResponse userResponse = userService.registerUser(userRequest);
         return userResponse;
-   }
+    }
 }
